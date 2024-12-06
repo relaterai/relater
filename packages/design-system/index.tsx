be7f5d40510a8544b5ce685238eaebaed4269b06
tsui66 <quinton.xu@gmail.com>
2024-12-06 14:56:07 +0800

    refactor(design-system): remove AuthProvider and update dependencies

--- packages/design-system/index.tsx
+++ packages/design-system/index.tsx
@@ -1,6 +1,6 @@
 import { AnalyticsProvider } from '@repo/analytics';
-import { AuthProvider } from '@repo/auth/provider';
 import { env } from '@repo/env';
+// @ts-expect-error
 import { VercelToolbar } from '@vercel/toolbar/next';
 import type { ThemeProviderProps } from 'next-themes';
 import { Toaster } from './components/ui/sonner';
@@ -14,12 +14,10 @@ export const DesignSystemProvider = ({
   ...properties
 }: DesignSystemProviderProperties) => (
   <ThemeProvider {...properties}>
-    <AuthProvider>
-      <AnalyticsProvider>
-        <TooltipProvider>{children}</TooltipProvider>
-        <Toaster />
-        {env.NODE_ENV === 'development' && <VercelToolbar />}
-      </AnalyticsProvider>
-    </AuthProvider>
+    <AnalyticsProvider>
+      <TooltipProvider>{children}</TooltipProvider>
+      <Toaster />
+      {env.NODE_ENV === 'development' && <VercelToolbar />}
+    </AnalyticsProvider>
   </ThemeProvider>
 );
