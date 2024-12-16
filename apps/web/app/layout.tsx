a433a096d3f9b5981402cca343943388b2cc942d
tsui66 <quinton.xu@gmail.com>
2024-12-16 21:48:32 +0800

    refactor(web): update web app configuration and dependencies

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,25 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+import '@repo/design-system/styles/globals.css';
+import './styles/web.css';
+import { DesignSystemProvider } from '@repo/design-system';
+import { fonts } from '@repo/design-system/lib/fonts';
+import type { ReactNode } from 'react';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+
+type RootLayoutProperties = {
+  readonly children: ReactNode;
+};
+
+const RootLayout = ({ children }: RootLayoutProperties) => (
+  <html lang="en" className={fonts} suppressHydrationWarning>
+    <body>
+      <DesignSystemProvider>
+        <Header />
+        {children}
+        <Footer />
+      </DesignSystemProvider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
