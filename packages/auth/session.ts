ff8505f7f452a266a8f3672585682b37401b2c9b
tsui66 <quinton.xu@gmail.com>
2024-12-17 17:58:26 +0800

    feat(auth): implement API key authentication and session handling

--- packages/auth/session.ts
+++ packages/auth/session.ts
@@ -5,6 +5,9 @@ import {
   getSearchParams,
 } from './lib/utils';
 import { auth } from './server';
+import { hashToken } from './hash-token';
+import prisma from '@repo/database';
+import { waitUntil } from '@vercel/functions';
 
 type WithSessionHandler = ({
   req,
@@ -25,13 +28,86 @@ export const withSession = (handler: WithSessionHandler) =>
         { params = {} }: { params: Record<string, string> | undefined }
     ) => {
       try {
-        const session = (await auth()) as Session;
-        if (!session || !session?.user || !session?.user?.id) {
-          throw new LaterApiError({
-            code: 'unauthorized',
+        let session: Session | null = null;
+        const authorizationHeader = req.headers.get("Authorization");
+        if (authorizationHeader) {
+          if (!authorizationHeader.includes("Bearer ")) {
+            throw new LaterApiError({
+              code: "bad_request",
+              message:
+                "Misconfigured authorization header. Did you forget to add 'Bearer '? Learn more: https://later.run/docs/auth",
+            });
+          }
+          const apiKey = authorizationHeader.replace("Bearer ", "");
+
+          const hashedKey = await hashToken(apiKey);
+
+          const user = await prisma.user.findFirst({
+            where: {
+              tokens: {
+                some: {
+                  hashedKey,
+                },
+              },
+            },
+            select: {
+              id: true,
+              name: true,
+              email: true,
+            },
+          });
+          if (!user) {
+            throw new LaterApiError({
+              code: "unauthorized",
+              message: "Unauthorized: Invalid API key.",
+            });
+          }
+
+          // TODO: Implement rate limiting
+          // const { success, limit, reset, remaining } = await ratelimit(
+          //   600,
+          //   "1 m",
+          // ).limit(apiKey);
+
+          // headers = {
+          //   "Retry-After": reset.toString(),
+          //   "X-RateLimit-Limit": limit.toString(),
+          //   "X-RateLimit-Remaining": remaining.toString(),
+          //   "X-RateLimit-Reset": reset.toString(),
+          // };
+
+          // if (!success) {
+          //   return new Response("Too many requests.", {
+          //     status: 429,
+          //     headers,
+          //   });
+          // }
+
+          waitUntil(
+            prisma.token.update({
+              where: {
+                hashedKey,
+              },
+              data: {
+                lastUsed: new Date(),
+              },
+            }),
+          );
+          session = {
+            user: {
+              id: user.id,
+              name: user.name || "",
+              email: user.email || "",
+            },
+          };
+        } else {
+          session = await auth();
+          if (!session || !session?.user || !session?.user?.id) {
+            throw new LaterApiError({
+              code: 'unauthorized',
             message: 'Unauthorized: Login required.',
           });
-        }
+        }}
         const searchParams = getSearchParams(req.url);
         return await handler({ req, params, searchParams, session });
       } catch (error) {
