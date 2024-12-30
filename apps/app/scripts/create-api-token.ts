0b1ff89cb440388ff19ec51b563bb5362f926e16
tsui66 <quinton.xu@gmail.com>
2024-12-31 00:22:29 +0800

    feat: add scripts for API token creation and execution

---
+++ apps/app/scripts/create-api-token.ts
@@ -0,0 +1,40 @@
+import { hashToken } from '@repo/auth/hash-token';
+import prisma from '@repo/database';
+import { nanoid } from '@repo/utils';
+
+async function main() {
+  const token = nanoid(24);
+  console.log({ token });
+  const user = await prisma.user.upsert({
+    where: {
+      id: 'cm5ant50d0000180q277dpbd0',
+    },
+    update: {},
+    create: {
+      email: 'test@later.run',
+    },
+  });
+  const hashedKey = await hashToken(token);
+  // take first 3 and last 4 characters of the key
+  const partialKey = `${token.slice(0, 3)}...${token.slice(-4)}`;
+  const existingToken = await prisma.token.findFirst({
+    where: {
+      userId: user.id,
+    },
+  });
+  if (existingToken) {
+    console.log('Token already exists');
+    return existingToken;
+  } else {
+    return await prisma.token.create({
+      data: {
+        name: 'E2E Test Key',
+        hashedKey,
+        partialKey,
+        userId: user.id,
+      },
+    });
+  }
+}
+
+main();
