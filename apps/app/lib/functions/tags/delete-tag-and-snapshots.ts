31ec7f5542922c33cb6781c8640e296a5f91f4fd
tsui66 <quinton.xu@gmail.com>
2025-01-14 18:11:19 +0800

    refactor(tags): simplify delete tag and snapshots logic

--- apps/app/lib/functions/tags/delete-tag-and-snapshots.ts
+++ apps/app/lib/functions/tags/delete-tag-and-snapshots.ts
@@ -7,67 +7,36 @@ export async function deleteTagAndSnapshots({
   id,
   userId,
 }: z.infer<typeof deleteTagSchema> & { userId: string }) {
-  // const tag = await prisma.$transaction(async (tx) => {
-  //   await tx.tag.update({
-  //     where: {
-  //       id,
-  //       userId,
-  //     },
-  //     data: {
-  //       snapshots: {
-  //         updateMany: {
-  //           where: {
-  //             userId,
-  //           },
-  //           data: {
-  //             isDeleted: true,
-  //             deletedAt: new Date(),
-  //           },
-  //         },
-  //       },
-  //     },
-  //   });
-  //   return await tx.tag.update({
-  //     where: {
-  //       id,
-  //       userId,
-  //     },
-  //     data: {
-  //       isDeleted: true,
-  //       deletedAt: new Date(),
-  //     },
-  //   });
-  // });
-
-  const tag = await prisma.tag.update({
-    where: {
-      id,
-      userId,
-    },
-    data: {
-      snapshots: {
-        updateMany: {
-          where: {
-            userId,
-          },
-          data: {
-            isDeleted: true,
-            deletedAt: new Date(),
+  const tag = await prisma.$transaction(async (tx) => {
+    await tx.tag.update({
+      where: {
+        id,
+        userId,
+      },
+      data: {
+        snapshots: {
+          updateMany: {
+            where: {
+              userId,
+            },
+            data: {
+              isDeleted: true,
+              deletedAt: new Date(),
+            },
           },
         },
       },
-    },
-  });
-
-  await prisma.tag.update({
-    where: {
-      id,
-      userId,
-    },
-    data: {
-      isDeleted: true,
-      deletedAt: new Date(),
-    },
+    });
+    return await tx.tag.update({
+      where: {
+        id,
+        userId,
+      },
+      data: {
+        isDeleted: true,
+        deletedAt: new Date(),
+      },
+    });
   });
 
   return {
